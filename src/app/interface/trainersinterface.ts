304a7f3e3118e3c2691e8c4ffb48066b597eb817
Davidos56 <wieczorek131@gmail.com>
2020-04-23 20:20:50 +0200

    change team

--- src/app/interface/trainersinterface.ts
+++ src/app/interface/trainersinterface.ts
@@ -2,6 +2,10 @@ export interface ITrainer {
     fullname: string,
     title: string,
     smallbackgroundString: string
+    content: ITrainerContent[];
+}
+
+export interface ITrainerContent {
     bigbackgroundString: string
     content: string
-}
+} 
